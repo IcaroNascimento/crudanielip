98dcceead7aa799c91ac663b74fdc3ee37b63fd7
IcaroNascimento <icaronascimento1@gmail.com>
2018-05-26 02:27:14 -0300

    Criacao do projeto

---
+++ src/main/java/com/icaronascimento/crudanielip/CrudanielipApplication.java
@@ -0,0 +1,12 @@
+package com.icaronascimento.crudanielip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudanielipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudanielipApplication.class, args);
+	}
+}
